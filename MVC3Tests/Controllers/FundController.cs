3976ef7e6f29304302abee22f81814609c23b9bd
Rich Linnell <richlinnell@gmail.com>
2012-10-23 10:06:41 +0100

    Building a basic framework

---
+++ MVC3Tests/Controllers/FundController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC3Tests.Controllers
+{
+    public class FundController : Controller
+    {
+
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// GET: /Fund/Create
+
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //} 
+
+        //
+        // POST: /Fund/Create
+
+        //[HttpPost]
+        //public ActionResult Create(FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add insert logic here
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+        
+        //
+        // GET: /Fund/Edit/5
+ 
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Fund/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+ 
+                return RedirectToAction("Index","Home");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        //// GET: /Fund/Delete/5
+ 
+        //public ActionResult Delete(int id)
+        //{
+        //    return View();
+        //}
+
+        ////
+        //// POST: /Fund/Delete/5
+
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    try
+        //    {
+        //        // TODO: Add delete logic here
+ 
+        //        return RedirectToAction("Index");
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
--- MVC3Tests/Controllers/HomeController.cs
+++ MVC3Tests/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC3Tests.Models;
 
 namespace MVC3Tests.Controllers
 {
@@ -11,8 +12,26 @@ namespace MVC3Tests.Controllers
         public ActionResult Index()
         {
             ViewBag.Message = "Welcome to DevExpress Extensions for ASP.NET MVC!";
-
-            return View();
+            var model = new List<Fund>
+                            {
+                                new Fund()
+                                    {
+                                        Code = "DKD",
+                                        ID = 1,
+                                        Name = "DK Distressed",
+                                        ManagerID = 1,
+                                        StrategyID = 1,
+                                    },
+                                new Fund()
+                                    {
+                                        Code = "LakeW",
+                                        ID = 2,
+                                        ManagerID = 2,
+                                        Name = "Lakewood",
+                                        StrategyID = 2,
+                                    }
+                            };
+            return View(model);
         }
     }
 }

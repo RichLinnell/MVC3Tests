945e65f76cc675d5421a621903b6c51788243bd7
Rich Linnell <richlinnell@gmail.com>
2012-10-23 13:23:21 +0100

    Playing with model validation
    interesting stuff on Model validation

--- MVC3Tests/Models/Fund.cs
+++ MVC3Tests/Models/Fund.cs
@@ -1,11 +1,22 @@
-﻿namespace MVC3Tests.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace MVC3Tests.Models
 {
+    [Bind(Exclude = "ID")]
     public class Fund
     {
+        [ScaffoldColumn(false)]
         public long ID { get; set; }
+        [Required(ErrorMessage = "Funds must have a name.")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Funds must have a code.")]
+        [StringLength(10)]
         public string Code { get; set; }
+        [DisplayName("Manager")]
         public long ManagerID { get; set; }
+        [DisplayName("Strategy")]
         public long StrategyID { get; set; }
     }
 }
